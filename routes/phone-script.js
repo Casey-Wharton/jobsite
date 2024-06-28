a0278e2789b8807425c97d02e0fab00ac1f1d561
Casey <caseywdevelopment@gmail.com>
2024-06-28 16:55:26 -0400

    Added partials

--- routes/phone-script.js
+++ routes/phone-script.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('index')
+    res.render('phone-script/phone-script')
 });
 
 module.exports = router;
