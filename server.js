71030a33ba3e51cf4e00880c5a9afeb274622f4b
Casey <caseywdevelopment@gmail.com>
2024-07-01 12:36:04 -0400

    User Update

--- server.js
+++ server.js
@@ -5,15 +5,20 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index');
 const phoneRouter = require('./routes/phone-script');
+const userRouter = require('./routes/users');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+// No idea what this does
+app.use(express.urlencoded({ limit: '10mb', extended: false }));
+app.use(express.json());
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL)
@@ -23,5 +28,6 @@ db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter);
 app.use('/phone-script', phoneRouter);
+app.use('/users', userRouter);
 
 app.listen(process.env.PORT || 3000);
